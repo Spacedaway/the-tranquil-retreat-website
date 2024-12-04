13b5a6cf13523494b748e2b069b353177f1ab5c1
Spacedaway <ekanabasiisok@gmail.com>
2024-12-04 08:54:37 +0100

    feat: add Spinner and SpinnerMini components with loading indicators

---
+++ app/hotels/loading.js
@@ -0,0 +1,11 @@
+import Spinner from "@/app/_components/Spinner";
+
+function Loading() {
+	return (
+		<div className="grid place-items-center">
+			<Spinner />
+			<p className="text-xl text-primary-200">Loading Hotels data...</p>
+		</div>
+	);
+}
+export default Loading;
