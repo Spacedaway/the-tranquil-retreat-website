12f4863183d4a68fd2ac5aebabbc369c64fc9ce0
Spacedaway <ekanabasiisok@gmail.com>
2024-12-08 23:31:52 +0100

    feat: implement TextExpander component for hotel description and highlight active navigation link in SideNavigation

--- app/hotels/[hotelId]/page.js
+++ app/hotels/[hotelId]/page.js
@@ -1,6 +1,7 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { getHotel, getHotels } from "../../_lib/data-service";
 import Image from "next/image";
+import TextExpander from "../../_components/TextExpander";
 
 export async function generateMetadata({ params }) {
 	const hotel = await getHotel(params.hotelId);
@@ -33,7 +34,9 @@ export default async function Page({ params }) {
 						Hotel {name}
 					</h3>
 
-					<p className="text-lg text-primary-300 mb-10">{description}</p>
+					<p className="text-lg text-primary-300 mb-10">
+						<TextExpander>{description}</TextExpander>
+					</p>
 
 					<ul className="flex flex-col gap-4 mb-7">
 						<li className="flex gap-3 items-center">
