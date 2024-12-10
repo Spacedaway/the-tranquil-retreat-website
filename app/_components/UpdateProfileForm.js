9a9ee26fdd73c0f9214ae178949e7fbbc398178c
Spacedaway <ekanabasiisok@gmail.com>
2024-12-10 15:31:04 +0100

    feat: implement delete reservation functionality, update profile form to handle guest data, and refactor components for improved user experience

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,26 +1,35 @@
 "use client";
 import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
-export function UpdateProfileForm({ children }) {
+export function UpdateProfileForm({ children, guest }) {
 	const [count, setCount] = useState(1);
-	// CHANGE
-	const countryFlag = "pt.jpg";
+
+	const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
 	return (
-		<form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+		<form
+			action={updateGuest}
+			className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+		>
 			<div className="space-y-2">
-				<label>Full name</label>
+				<label htmlFor="fullName">Full name</label>
 				<input
-					disabled
-					className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+					readOnly
+					defaultValue={fullName}
+					name="fullName"
+					className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm read-only:cursor-not-allowed read-only:bg-gray-600 read-only:text-gray-400"
 				/>
 			</div>
 
 			<div className="space-y-2">
 				<label>Email address</label>
 				<input
-					disabled
-					className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+					readOnly
+					defaultValue={email}
+					name="email"
+					className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm read-only:cursor-not-allowed read-only:bg-gray-600 read-only:text-gray-400"
 				/>
 			</div>
 
@@ -40,16 +49,28 @@ export function UpdateProfileForm({ children }) {
 			<div className="space-y-2">
 				<label htmlFor="nationalID">National ID number</label>
 				<input
+					defaultValue={nationalID}
 					name="nationalID"
 					className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
 				/>
 			</div>
 
 			<div className="flex justify-end items-center gap-6">
-				<button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-					Update profile
-				</button>
+				<Button />
 			</div>
 		</form>
+	);
+}
+
+function Button() {
+	const { pending } = useFormStatus();
+
+	return (
+		<button
+			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+			disabled={pending}
+		>
+			{pending ? "Updating..." : "Update profile"}
+		</button>
 	);
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,9 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
 	await signIn("google", { redirectTo: "/account" });
@@ -8,4 +11,55 @@ export async function signInAction() {
 
 export async function signOutAction() {
 	await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuest(formData) {
+	const alphanumericRegex = /^[a-zA-Z0-9]{6,12}$/;
+	const session = await auth();
+
+	if (!session) throw new Error("You must be logged in");
+
+	const nationalID = formData.get("nationalID");
+	const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+	if (!alphanumericRegex.test(nationalID))
+		throw new Error(
+			"National ID must be alphanumeric and between 6 and 12 characters"
+		);
+
+	const updateData = { nationality, countryFlag, nationalID };
+
+	const { data, error } = await supabase
+		.from("guests")
+		.update(updateData)
+		.eq("id", session.user.guestId)
+		.select()
+		.single();
+
+	if (error) throw new Error("Guest could not be updated");
+
+	revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+	const session = await auth();
+
+	if (!session) throw new Error("You must be logged in");
+
+	const guestBookings = await getBookings(session.user.guestId);
+	const bookingExists = guestBookings.some(
+		(booking) => booking.id === bookingId
+	);
+
+	if (!bookingExists) throw new Error("Reservation does not exist");
+
+	const { data, error } = await supabase
+		.from("bookings")
+		.delete()
+		.eq("id", bookingId)
+		.single();
+
+	if (error) throw new Error("Reservation could not be deleted");
+
+	revalidatePath("/account/reservations");
 }
