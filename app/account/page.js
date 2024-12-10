47e2c67fe53d774e751cb39bc8ac303b9dc01100
Spacedaway <ekanabasiisok@gmail.com>
2024-12-10 13:03:16 +0100

    feat: implement sign-in and sign-out functionality with Google authentication, create actions for sign-in and sign-out, and update account and login pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,18 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
 	title: "Account",
 };
 
-function AccountPage() {
+async function AccountPage() {
+	const session = await auth();
+	console.log("🚀 ~ AccountPage ~ session:", session);
+	const user = session?.user?.name.split(" ")[0];
+
 	return (
 		<div>
 			<h2 className="font-semibold text-2xl text-accent-400 mb-7">
-				Welcome, Jonas
+				Welcome, {user}!
 			</h2>
 		</div>
 	);
